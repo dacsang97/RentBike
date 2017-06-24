2690c73412d2d65ce4e3c6c20783df108494f02d
HiepDz <hiephhse04809@fpt.edu.vn>
2017-06-24 15:46:49 +0700

    update map

--- RentBi/ViewController/MainViewController.swift
+++ RentBi/ViewController/MainViewController.swift
@@ -7,20 +7,80 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController , GMSMapViewDelegate , CLLocationManagerDelegate {
 
+    @IBOutlet var btnView: UIView!
     @IBOutlet var mapView: GMSMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 13.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        // User Location
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        self.view.insertSubview(mapView, at: 0)
+        self.view.bringSubview(toFront: btnView)
+        self.view.insertSubview(btnView, at: 1)
+        for btn in btnOutlet {
+           // self.view.insertSubview(btn, at: 1)
+            
+        }
+        
+
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        self.view.insertSubview(mapView, at: 0)
+        self.view.bringSubview(toFront: btnView)
+        for btn in btnOutlet {
+            btn.layer.cornerRadius = 20
+          //  self.view.insertSubview(btn, at: 1)
+            
+        }
+        
+        
     }
+    
+//    //Location Manager delegates
+//    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        
+//        let location = locations.last
+//        
+//        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 17.0)
+//        
+//        self.mapView?.animate(to: camera)
+//        
+//        //Finally stop updating location otherwise it will come again and again in this delegate
+//        self.locationManager.stopUpdatingLocation()
+//        
+//    }
+    
+    @IBOutlet var btnOutlet: [UIButton]!
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.last
+        let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
+                                              longitude: userLocation!.coordinate.longitude, zoom: 13.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        self.view = mapView
+        
+        locationManager.stopUpdatingLocation()
+        
+        
+        
+    }
+
     
 
     /*
