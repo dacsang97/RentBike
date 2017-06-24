a8d17fb9f252f1b65c20fdcc98a9ad799792e820
HiepDz <hiephhse04809@fpt.edu.vn>
2017-06-24 14:19:19 +0700

    up pod + MVC struc

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RentBi' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RentBi
+	 pod 'GoogleMaps'
+  pod 'GooglePlaces'
+
+  target 'RentBiTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RentBiUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ RentBi/ViewController/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  RentBi
+//
+//  Created by mac on 6/24/17.
+//  Copyright © 2017 vteam. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var mapView: GMSMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
